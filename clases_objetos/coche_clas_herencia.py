080debab48d066d6c98a8ea6999b07386dfbff06
kydiazpe <kdiazped@emeal.nttdata.com>
2023-10-10 17:51:50 -0500

    herencia multiple

--- clases_objetos/coche_clas_herencia.py
+++ clases_objetos/coche_clas_herencia.py
@@ -14,14 +14,19 @@ class Coche:
         return self.__encendido
 
 
+class Coche4x4:
+    size_rueda = 19
+
+
 # herencia de una clase a otra
-class CocheDeportivo(Coche):
+class CocheDeportivo(Coche, Coche4x4):
     cv = 60
 
-    def __init__(self, marca, color, cv):
+    def __init__(self, marca, color, cv, size_rueda):
         self.marca = marca
         self.color = color
         self.cv = cv
+        self.size_rueda = size_rueda
 
 
 """Aca se esta instanciando la clase"""
@@ -31,8 +36,8 @@ coche1 = Coche("Toyota", "Negro")
 coche1.encender()
 # coche1.__encendido
 
-coche_lujo = CocheDeportivo("BMW", "Negro", 200)
-print(f'Marca: {coche_lujo.marca}, Color: {coche_lujo.color}, CV: {coche_lujo.cv}')
+coche_lujo = CocheDeportivo("BMW", "Negro", 200, 20)
+print(f'Marca: {coche_lujo.marca}, Color: {coche_lujo.color}, CV: {coche_lujo.cv}, tamaño rueda: {coche_lujo.size_rueda}')
 
 print(f'Marca: {coche1.marca}, Color: {coche1.color}')
 print(f'Encendido: {coche1.get_encendido()}')
